3cef45aa3b0cbfa225634b332e4cb591144ad9f8
Shaik Sajid Hussain <sanjusazid0@gmail.com>
2025-12-26 21:25:14 +0530

    fix: remove process.exit on MongoDB connection failure and use env variables

--- config/db.js
+++ config/db.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://sanjusazid41:8kILKkYcDKFUTN9P@cluster0.fdb48.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        const mongoURI = process.env.MONGODB_URI || "mongodb+srv://sanjusazid41:8kILKkYcDKFUTN9P@cluster0.fdb48.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+        await mongoose.connect(mongoURI);
         console.log('MongoDB connected successfully');
     } catch (error) {
-        console.error('MongoDB connection error:', error);
-        process.exit(1);
+        console.error('MongoDB connection error:', error.message);
+        // Don't exit on serverless - just log the error
+        // The app will still run, but without DB functionality
     }
 };
 
